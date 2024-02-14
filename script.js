11441d7a6a25b1ef554263f757000742ac42f695
Roxaski <145111435+Roxaski@users.noreply.github.com>
2024-02-14 16:35:32 +0000

    various improvements and adjustments

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ let menu = document.querySelector('.hamburger-menu');
 menu.addEventListener('click', () => {
     wrapper.classList.toggle('active')
 });
-
+//disable scroll when nav menu is open
 menu.addEventListener('click', () => {
     document.body.classList.toggle('no-scroll');
 });
-//hamburger menu & no scroll end//
