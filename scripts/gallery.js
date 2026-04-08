39d438d26c18da5b64bf6b255b5596cb47f7a8d7
Roxas Ski <roxaskiGithub@gmail.com>
2026-04-08 09:42:26 +0100

    updated code for improved accessibility and performance

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,192 +1,235 @@
+const navElements = document.querySelectorAll('nav a');
 const gallery = document.querySelector('.gallery');
 const galleryImgs = document.querySelectorAll('.gallery img');
+const overlay = document.querySelector('.gallery-overlay');
 const lightbox = document.querySelector('.lightbox img');
-const lightboxSizes = '(max-width: 865px) 90vw, 600px';
 const previousBtn = document.querySelector('.previous');
 const nextBtn = document.querySelector('.next');
-const overlay = document.querySelector('.gallery-overlay');
 
-// stores the index of the currently displayed image
-let currentImage;
+// keeps track of which image was clicked
+let currentImg = 0;
 
-// listens out for a click on the gallery
-gallery.addEventListener('click', (e) => {
-    // check if the click is on a gallery image
-    if (e.target.tagName === 'IMG') {
-        // checks which image was clicked from the array of gallery images
-        currentImage = Array.from(galleryImgs).indexOf(e.target);
+// creates new image variables in order to preload them when the lightbox is active
+let preloadPreviousImg = new Image();
+let preloadNextImg = new Image();
 
-        displayOverlay();
-        imagePreview();
-        preload(currentImage + 1);
-        preload(currentImage - 1);
-    };
-});
+// creates an array of the images within the gallery
+const galleryImgArray = Array.from(galleryImgs);
 
-// listens out for the enter key on the gallery
-gallery.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter' && e.target.tagName === 'IMG') {
-        currentImage = Array.from(galleryImgs).indexOf(e.target);
+/*
+    waits for the image to be fully decoded before displaying it,
+    this is done to prevent firefox from briefly flashing the previous image if one was clicked on
+*/
+async function setLightboxImg() {
+    const img = galleryImgArray[currentImg];
+    
+    lightbox.src = img.src;
+    lightbox.srcset = img.srcset;
+    
+    await lightbox.decode();
+};
 
-        displayOverlay();
-        imagePreview();
-        preload(currentImage + 1);
-        preload(currentImage - 1);
-    };
-});
+/*
+    displays the lightbox along with disabling the scroll while it's open,
+    along with disabling the respective buttons from the first and last lightbox image,
+    and setting the tab index to -1 in order to prevent the gallery images from being tabbed
+*/
+function openLightBox(e) {
+    // checks if the target is an image within the gallery
+    if(e.target.tagName === 'IMG') {
+        document.body.classList.add('lightbox-no-scroll');
 
-// preloads the previous and next image
-function preload(imageIndex) {
-    // checks if the image is within the length of the gallery 
-    if (imageIndex >= 0 && imageIndex < galleryImgs.length) {
-        const img = galleryImgs[imageIndex];
-        const preloadImg = new Image();
+        // stores the value of the current image that was clicked on from the array
+        currentImg = galleryImgArray.indexOf(e.target);
 
-        // sets the image src, srcset and size for the browser to preload
-        preloadImg.srcset = img.srcset;
-        preloadImg.sizes = lightboxSizes;
-        preloadImg.src = img.src;
+        setLightboxImg();
+        
+        overlay.style.transition = 'opacity 150ms ease';
+
+        overlay.classList.add('active');
+        lightbox.classList.add('active');
+
+        // loops through each element and sets the tab index accordingly
+        navElements.forEach(link => {
+            link.setAttribute('tabIndex', '-1');
+        });
+
+        galleryImgArray.forEach(img => {
+            img.setAttribute('tabIndex', '-1');
+        });
+
+        lightboxBtns();
     };
 };
 
-// displays an overlay
-function displayOverlay() {
-    document.body.style.overflow = 'hidden';
-    overlay.style.display = 'block';
-};
-
-// displays image & buttons
-function imagePreview() {
-    lightbox.classList.add('active');
-
-    // lightbox should use more appropriate image sizes
-    lightbox.srcset = galleryImgs[currentImage].srcset;
-    lightbox.sizes = lightboxSizes;
-    lightbox.alt = galleryImgs[currentImage].alt;
-
-    // fallback in case srcset and sizes isn't supported on the browser
-    lightbox.src = galleryImgs[currentImage].src;
-
-    displayGalleryBtns();
-};
-
-// removes the buttons from the first and last lightbox images
-function displayGalleryBtns() {
-    if(currentImage == 0) {
-        previousBtn.style.display = 'none';
+// hides one of the lightbox buttons depending on the position of the image within the array
+function lightboxBtns() {
+    if(currentImg === 0) {
+        previousBtn.classList.remove('active');
     } else {
-        previousBtn.style.display = 'block';
+        previousBtn.classList.add('active');
     };
 
-    if(currentImage == galleryImgs.length - 1) {
-        nextBtn.style.display = 'none';
+    if(currentImg === galleryImgArray.length - 1) {
+        nextBtn.classList.remove('active');
     } else {
-        nextBtn.style.display = 'block';
+        nextBtn.classList.add('active');
     };
 };
 
-// displays next image
-nextBtn.addEventListener('click', () => {
-    currentImage ++;
-    imagePreview();
-    preload(currentImage +1);
-});
+// preloads the adjacent photos within the lightbox
+function preloadAdjacentImgs() {
+    if (currentImg > 0) {
+        preloadPreviousImg.src = galleryImgArray[currentImg - 1].src;
+    };
 
-// displays previous image
-previousBtn.addEventListener('click', () => {
-    currentImage --;
-    imagePreview();
-    preload(currentImage - 1);
-});
+    if (currentImg < galleryImgArray.length - 1) {
+        preloadNextImg.src = galleryImgArray[currentImg + 1].src;
+    };
+};
 
-// removes overlay, image buttons, src and srcset along with alt
-function closeOverlay() {
-    document.querySelector('body').style.overflow = 'auto';
+/*
+    closes the lightbox by removing the active classes and re-enabling scroll,
+    while also setting the tab index back to 0 to allow the images in the gallery to be tabbed to
+*/
+function closeLightbox () {
+    document.body.classList.remove('lightbox-no-scroll');
+    overlay.style.transition = 'none';
+    overlay.classList.remove('active');
+    lightbox.classList.remove('active');
     lightbox.src = '';
     lightbox.srcset = '';
-    lightbox.alt = '';
-    lightbox.classList.remove('active');
-    overlay.style.display = 'none';
-    nextBtn.style.display = 'none';
-    previousBtn.style.display = 'none';
+    previousBtn.classList.remove('active');
+    nextBtn.classList.remove('active');
+
+    // loops through each element and sets the tab index accordingly
+    navElements.forEach(link => {
+        link.setAttribute('tabIndex', 0);
+    });
+
+    galleryImgArray.forEach(img => {
+        img.setAttribute('tabIndex', 0);
+    });
 };
 
-// closes the overlay
+// lightbox click event listeners for mouse
+gallery.addEventListener('click', (e) => {
+    openLightBox(e);
+    preloadAdjacentImgs();
+});
+
 overlay.addEventListener('click', () => {
-    closeOverlay();
+    closeLightbox();
 });
 
-// stores the value of the timeout ID which is used to delay the image resize from triggering too often
-let timeoutID;
+previousBtn.addEventListener('click', () => {
+    currentImg--;
 
-window.addEventListener('resize', () => {
-    // clears the timeoutID after it's initially triggered
-    clearTimeout(timeoutID);
-
-    // triggers the timeout only if the lightbox is displayed
-    if(overlay.style.display == 'block') {
-        // added active class to gallery to help with flickering during resize
-        gallery.classList.add('active');
-
-        // re-render image then show the content after a short delay set by the timeout below
-        timeoutID = setTimeout(() => {
-            imagePreview();
-
-            // removes the active class on the next repaint
-            requestAnimationFrame(() => {
-                gallery.classList.remove('active');
-            });
-        }, 100); // 100ms delay before the next resize can happen
-    }
+    setLightboxImg();
+    lightboxBtns();
+    preloadAdjacentImgs();
 });
 
-// stores the value of where the finger touches the screen
-let touchStart;
+nextBtn.addEventListener('click', () => {
+    currentImg++;
 
-lightbox.addEventListener('touchstart', (e) => {
-    // stops the default event from happening (swiping from the edge of the screen makes the page go back)
-    e.preventDefault();
-    
-    // returns the location of where the touch started on the X-axis
-    touchStart = e.touches[0].clientX;
+    setLightboxImg();
+    lightboxBtns();
+    preloadAdjacentImgs();
 });
 
-//  stores the value of when the finger stops on the screen
-let touchEnd;
+// lightbox click event listeners for keyboard
+window.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape' && lightbox.classList.contains('active')) {
+        closeLightbox();
+    };
 
-lightbox.addEventListener('touchend', (e) => {
-    // returns the location of where touch ended on the X-axis
-    touchEnd = e.changedTouches[0].clientX;
+    if(e.key === 'ArrowLeft' && lightbox.classList.contains('active')) {
+        if(currentImg === 0) {
+            return;
+        };
 
-    // defines the minimum distance needed to swipe for next and previous images
-    const minSwipe = 100;
-    // calculates the value to show the next or previous image
-    const swipeToChangeImg = touchStart - touchEnd;
+        currentImg--;
 
-    // if you swipe to the left and aren't on the last image, then it displays the next image
-    if(swipeToChangeImg > minSwipe && currentImage < galleryImgs.length - 1) {
-        currentImage ++;
-        imagePreview();
-        preload(currentImage + 1);
-    // Else if you swipe to the right and aren't on the first image, then it displays the previous image
-    } else if (swipeToChangeImg < -minSwipe && currentImage > 0) {
-        currentImage --;
-        imagePreview();
-        preload(currentImage - 1);
+        setLightboxImg();
+        lightboxBtns();
+        preloadAdjacentImgs();
+
+    } else if (e.key === 'ArrowRight' && lightbox.classList.contains('active')) {
+        if(currentImg === galleryImgArray.length - 1) {
+            return;
+        };
+
+        currentImg++;
+
+        setLightboxImg();
+        lightboxBtns();
+        preloadAdjacentImgs();
     };
 });
 
-// image navigation using the arrows keys
-window.addEventListener('keydown', (e) => {
-    if(overlay.style.display == 'block' && e.key == 'ArrowRight' &&  currentImage < galleryImgs.length - 1) {
-        currentImage ++;
-        imagePreview();
-        preload(currentImage + 1);
-    } else if (overlay.style.display == 'block' && e.key == 'ArrowLeft' && currentImage > 0) {
-        currentImage --;
-        imagePreview();
-        preload(currentImage - 1);
-    } else if (e.key == 'Escape') {
-        closeOverlay();
+gallery.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter' || e.key === ' ') {
+        e.preventDefault();
+        openLightBox(e);
+        preloadAdjacentImgs();
+    };
+});
+
+// keeps track of where the screen tapping starts and ends, along with setting img zoom to false
+let screenTapStart;
+let screenTapEnd;
+let imgZoom = false;
+
+lightbox.addEventListener('touchstart', (e) => {
+    // keeps track of whether more than one finger is on the screen
+    if(e.touches.length > 1) {
+        imgZoom = true;
+        return;
+    } else {
+        imgZoom = false;
+    };
+
+    // the position of the initial tap on the screen
+    screenTapStart = e.touches[0].clientX;
+});
+
+lightbox.addEventListener('touchend', (e) => {
+    // if there no fingers on the screen then the variable is set to false
+    if (e.touches.length === 0) {
+        imgZoom = false;
+    };
+
+    // returns early if any fingers are still on the screen or if image zoom is active
+    if (e.touches.length > 0 || imgZoom) {
+        return;
+    };
+
+    // the position of where the finger lifted off the screen
+    screenTapEnd = e.changedTouches[0].clientX;
+
+    // checks if the swipe moved at least 100px to the left
+    if(screenTapEnd - screenTapStart <= -100 && lightbox.classList.contains('active')) {
+        
+        if(currentImg === galleryImgArray.length - 1) {
+            return;
+        };
+
+        currentImg++;
+
+        setLightboxImg();
+        preloadAdjacentImgs();
+
+    // checks if the swipe moved at least 100px to the right
+    } else if(screenTapEnd - screenTapStart >= 100 && lightbox.classList.contains('active')) {
+
+        if(currentImg === 0) {
+            return;
+        };
+
+        currentImg--;
+
+        setLightboxImg();
+        preloadAdjacentImgs();
     };
 });
